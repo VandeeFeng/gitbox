a0fa327fd67408b612d59eb0cdd68cd3e2597111
Vandee <102362163+VandeeFeng@users.noreply.github.com>
2024-07-18 19:45:28 +0800

    Update summary.js

--- codes/summary.js
+++ codes/summary.js
@@ -23,7 +23,7 @@ async function typeWriter(text, elementId) {
       if (index < text.length) {
         element.textContent += text.charAt(index);
         index++;
-        setTimeout(writeLetter, 500); // 调整时间来控制打字速度
+        setTimeout(writeLetter, 1500); // 调整时间来控制打字速度
       }
     };
     writeLetter();
