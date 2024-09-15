44de0955225e171926774a31b43b01525db6067b
Vandee <102362163+VandeeFeng@users.noreply.github.com>
2024-09-15 16:45:42 +0800

    Update summary.js

--- codes/summary.js
+++ codes/summary.js
@@ -62,9 +62,9 @@ async function ai_gen() {
   }
 
   if (ogUrl) {
-    const checkUploadedUrl = `https://summary.vandee.art/is_uploaded?id=${encodeURIComponent(ogUrl)}&sign=${postContentSign}`;
-    const getSummaryUrl = `https://summary.vandee.art/get_summary?id=${encodeURIComponent(ogUrl)}&sign=${postContentSign}`;
-    const uploadBlogUrl = new URL("https://ssummary.vandee.art/upload_blog");
+    const checkUploadedUrl = `https://y.demo.wvusd.homes/https://summary.vandee.art/is_uploaded?id=${encodeURIComponent(ogUrl)}&sign=${postContentSign}`;
+    const getSummaryUrl = `https://y.demo.wvusd.homes/https://summary.vandee.art/get_summary?id=${encodeURIComponent(ogUrl)}&sign=${postContentSign}`;
+    const uploadBlogUrl = new URL("https://y.demo.wvusd.homes/https://summary.vandee.art/upload_blog");
     uploadBlogUrl.search = new URLSearchParams({ id: encodeURIComponent(location.href) });
 
     try {
